c869795e64869e11716b506d2b25f6d085a257ed
noejunior792 <noedombaxe@gmail.com>
2024-11-24 12:47:47 +0100

    ❌fix: delete directory for mongoose

--- lib/models/discipline.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const disciplineSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  course: { type: mongoose.Schema.Types.ObjectId, ref: "Course", required: true },
-}, { timestamps: true });
-
-export const Discipline = mongoose.models.Discipline || mongoose.model("Discipline", disciplineSchema);
