025896cfdd5d647e11ee1230224b54f0fe8979a3
noejunior792 <noedombaxe@gmail.com>
2024-11-29 15:36:01 +0100

    up

--- lib/models/course.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const courseSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  institution: { type: mongoose.Schema.Types.ObjectId, ref: "Institution", required: true },
-}, { timestamps: true });
-
-export const Course = mongoose.models.Course || mongoose.model("Course", courseSchema);
